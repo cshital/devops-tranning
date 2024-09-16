5c606706ed625f4117bd67de98f3025c2831f81f
cshital <shitalchauhan549@gmail.com>
2024-09-16 23:43:31 +0530

    delete-day33

--- day33-Helm-Kubernetes-AWS/bucket-data/script.js
+++
@@ -1,9 +0,0 @@
-const image = document.querySelector(".image .image-2");
-const slider = document.querySelector(".slider input");
-const drag = document.querySelector(".slider .dragLine");
-
-slider.oninput = () =>{
-    let sliderVal = slider.value;
-    drag.style.left = sliderVal + "%";
-    image.style.width = sliderVal + "%";
-}
